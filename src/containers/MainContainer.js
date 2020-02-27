8201b314ac8bc88f7d3d1ae6a87877a642e751e6
annacarey <annacarey@icloud.com>
2020-02-27 13:51:16 -0500

    some changes

--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -5,6 +5,16 @@ class MainContainer extends React.Component {
     state = {
         paintings: []
     }
+
+    componentDidMount() {
+        fetch('http://localhost:3000/paintings')
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            console.log(data);
+        });
+    }
   
     render() {
         return (
