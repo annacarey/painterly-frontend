1b521328ac28decc219297b1ef8364e8af31c57d
annacarey <annacarey@icloud.com>
2020-02-27 13:22:17 -0500

    added main container

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
+import MainContainer from './containers/MainContainer'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          HELLO TESTING GITHUB
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <MainContainer />
     </div>
   );
 }
