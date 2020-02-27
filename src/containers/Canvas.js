7a659476379ba4fee16ba60c77477b896a0f7b26
Your Name <samanbatool08@gmail.com>
2020-02-27 15:32:35 -0500

    toolbar divs

--- src/containers/Canvas.js
+++ src/containers/Canvas.js
@@ -1,17 +1,25 @@
 import React from 'react';
-import Board from './components/Board'
-import Toolbar from './components/Toolbar'
+import Board from '../components/Board'
+import Toolbar from '../components/Toolbar'
 
 
 class Canvas extends React.Component {
   
-   
+   state = {
+       currentGrid: [],
+       currentColor: 'red'
+   }
   
+   setColorOnClick = (color) => {
+       this.setState({currentColor: color})
+   }
+
+   
     render() {
         return (
         <div>
            <Board />
-           <Toolbar />
+           <Toolbar setColor={this.setColorOnClick} currentColor={this.state.currentColor}/>
         </div>
         )
     }
