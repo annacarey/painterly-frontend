94aad61e6a3ca3f24b4a87a40fb99657bdef5e57
github username <github email address>
2020-02-27 13:22:17 -0500

    added main container

---
+++ src/containers/MainContainer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class MainContainer extends React.Component {
+  
+    state = {
+        paintings: []
+    }
+  
+    render() {
+        return (
+        <div>HELLO
+        </div>
+        )
+    }
+}
+
+export default MainContainer;
