5aa9a4f84ef20ebd4cffd2350d7678a8724e28f4
Your Name <samanbatool08@gmail.com>
2020-02-27 16:00:41 -0500

    fix merge conflict

--- src/components/Toolbar.js
+++ src/components/Toolbar.js
@@ -1,13 +1,22 @@
 import React from 'react';
 
-function Toolbar () {
-   
-        return (
-        <div>
-         
-        </div>
-        )
+
+function Toolbar (props) {
+
+
+return (
+<div> 
     
+<div onClick={() => props.setColor("red")} className="square">Red</div>
+<div onClick={() => props.setColor("blue")} className="square">Blue</div>
+<div onClick={() => props.setColor("green")} className="square">Green</div>
+<div onClick={() => props.setColor("yellow")} className="square">Yellow</div>
+<div onClick={() => props.setColor("orange")} className="square">Orange</div>
+<div onClick={() => props.setColor("purple")} className="square">Purple</div>
+
+</div>
+)
+
 }
 
 export default Toolbar;
