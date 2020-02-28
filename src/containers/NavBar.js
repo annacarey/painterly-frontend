f4a404c009ad5bf52f51533991ea759ad26e9ee3
Your Name <samanbatool08@gmail.com>
2020-02-28 10:32:06 -0500

    all paintings

--- src/containers/NavBar.js
+++ src/containers/NavBar.js
@@ -3,6 +3,7 @@ import React from 'react';
 function NavBar() {
   return (
     <div className="navbar">
+      <div className="nav-links">All Paintings</div>
       <div className="nav-links">Gallery</div>
       <div className="nav-links">Your Paintings</div>
       <div className="nav-links">Logout</div>
