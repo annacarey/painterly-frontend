6022abae5934f57118838d7b4536a6367881c9d9
github username <github email address>
2020-03-01 12:19:39 -0500

    implemented signup button in form

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import './Components.css';
 
 class Welcome extends React.Component  {
 
     state = {
         username: "",
-        password: ""
+        password: "",
+        signupToggle: false,
+        noUserFoundToggle: false
     }
 
     handleChange = (e) => {
@@ -15,32 +18,70 @@ class Welcome extends React.Component  {
         })
     }
 
-    handleSubmit = (e) => {
+    handleFormClick = (e) => {
         e.preventDefault()
-        fetch('http://localhost:3000/users')
-        .then((response) => {
-            return response.json()
-        })
-        .then((users) => {
-            const user = users.filter(user => user.username === this.state.username)[0]
-            this.props.getUser(user)
-        })
+        if (e.target.name  === "login") {
+            fetch('http://localhost:3000/users')
+            .then((response) => {
+                return response.json()
+            })
+            .then((users) => {
+                const user = users.filter(user => user.username === this.state.username)[0]
+                if (!user) {
+                    this.setState(() => {return {noUserFoundToggle: true}})
+                    console.log(this.state.noUserFoundToggle)
+                } else {
+                    this.props.getUser(user)
+                    this.setState(() => {return {noUserFoundToggle: false}})
+                }
+            })
+        } else if (e.target.name === "signup") {
+            console.log("We are int eh signup fetch")
+            fetch('http://localhost:3000/users',{
+            method: "POST",
+            headers: {'content-type': 'application/json',
+                    accepts: 'application/json'},
+            body: JSON.stringify({
+                username: this.state.username,
+                password: this.state.password
+            })}
+            )
+            .then(resp => resp.json())
+            .then(newUser => {
+                console.log(newUser)
+            })
+        }
     }
 
+    handleClick = () => [
+        this.setState(prevState => {return {signupToggle: !prevState.signupToggle}})
+    ]
+
+    displaySignupForm = 
+        <div className="signup-form">
+            <h2>Sign Up!</h2>
+            <form>
+                <input type="text" name="username"/>
+            </form>
+        </div>
+    
     render() {
         return (
-        <div class="login">
-            <form onSubmit={this.handleSubmit}>
+        <div className="welcome">
+            <form>
                 <label>Username: </label>
                 <input name="username" value={this.state.username} type="text" onChange={this.handleChange}></input>
                 <label> Password: </label>
                 <input name="password" value={this.state.password} type="text" onChange={this.handleChange}></input>
-                <input type="submit"/>
+                <input onClick={this.handleFormClick} type="submit" name="login" value="Log in"/>
+                <input onClick={this.handleFormClick} type="submit" name="signup" value="Sign up"/>
             </form>
+            <br></br>
+            {this.state.noUserFoundToggle && <p className="error">Username and/or password incorrect</p>}
+            <br></br>
         </div>
         )
     }
 }
-
 
 export default Welcome;
